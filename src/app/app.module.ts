42017c53a012d10f01eb19f66618ed26b38bc138
aurorasarkka <77781578+aurorasarkka@users.noreply.github.com>
2021-11-12 09:23:13 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatSliderModule} from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrainingComponent } from './training/training.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { CalculatorComponent } from './calculator/calculator.component' ;
+import {MatGridListModule} from "@angular/material/grid-list";
+import {FormsModule} from "@angular/forms";
+import { FeedbackpageComponent } from './feedbackpage/feedbackpage.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainingComponent,
+    CalculatorComponent,
+    FeedbackpageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatButtonModule,
+        MatSlideToggleModule,
+        FormsModule,
+      MatSliderModule,
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
